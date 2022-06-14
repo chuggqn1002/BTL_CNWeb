468e104ff3680f845e8374379c9dd9eab031cec0
nhoxkenk <nhadau12300@example.com>
2022-06-14 11:18:07 +0700

    popover

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log($("[data-toggle=popover]"))
+$(function () {
+    $("[data-toggle=popover]").popover({
+        placement: "right",
+        trigger: "manual",
+        sanitize: false,
+        html: true,
+        content: function () {
+            var content = $(this).attr("data-popover-content");
+            return $(content).children(".popover-body").html();
+        },
+        title: function () {
+            var title = $(this).attr("data-popover-content");
+            return $(title).children(".popover-heading").html();
+        }
+    })
+        .on("mouseenter", function () {
+            var _this = this;
+            $(this).popover("show");
+            $(".popover").on("mouseleave", function () {
+                $(_this).popover('hide');
+            });
+        }).on("mouseleave", function () {
+            var _this = this;
+            setTimeout(function () {
+                if (!$(".popover:hover").length) {
+                    $(_this).popover("hide");
+                }
+            }, 100);
+        });
+});
